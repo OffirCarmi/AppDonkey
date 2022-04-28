e993d8c9e01f9861c3901e2bd91eb88e96539ca3
Offir Carmi <offircarmi@gmail.com>
2022-04-28 16:35:01 +0300

    logo welcome

--- pages/welcome.jsx
+++ pages/welcome.jsx
@@ -4,7 +4,7 @@ function _Welcome({ history }) {
 
     return < section className="welcome" >
         <section className="welcome-msg">
-            <img src="../assets/img/logo.png" alt="" />
+            <img src="assets/img/logo.png" alt="" />
             <h1>AppDonkey!<span className="rtm">®️</span></h1>
             <h3>כי כל אחד צריך חמור עבודה לצידו</h3>
             {/* <h3>Have some free time, beacuse we don't</h3> */}
