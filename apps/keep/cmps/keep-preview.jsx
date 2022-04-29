918db4fe19e9f02fd76ca5629e206b50ea1014e2
Offir Carmi <offircarmi@gmail.com>
2022-04-29 11:06:00 +0300

    backup before big changes

--- apps/keep/cmps/keep-preview.jsx
+++ apps/keep/cmps/keep-preview.jsx
@@ -2,26 +2,72 @@ import { Txt } from './keep-types/txt.jsx'
 import { Img } from './keep-types/img.jsx'
 import { Todos } from './keep-types/todos.jsx'
 import { Video } from './keep-types/video.jsx'
+import { BgColor } from './dynamic-colors/bg-color.jsx'
+import { keepService } from '../services/keep.service.js'
+
+
 const { Link } = ReactRouterDOM
 export class KeepPreview extends React.Component {
+    state = {
+        showColors: false,
+       
+    }
+
+    onShowColors = () => {
+        event.stopPropagation()
+        this.setState({ showColors: !this.state.showColors })
+    }
+
+
+    onHideColors = () => {
+        event.stopPropagation()
+        this.setState({ showColors: !this.state.showColors })
+    }
+
+
 
     render() {
+        const { color } = this.state
         const { keep, onRemoveKeep } = this.props
         const { type, id } = keep
 
-        return <article key={id} className="keep-preview flex col space-between">
+        return <article style={{ backgroundColor: (!keep.color) ? 'White' : keep.color }} key={id} className="keep-preview flex col space-between">
             <section className="keep-body">
                 <_DynamicCmp keep={keep} />
             </section>
-            <aside className="keep-tools flex space-between">
-                <button>color</button>
+            <aside className="keep-tools">
+                <button onClick={this.onShowColors}>color</button>
                 <button>pin</button>
                 <button onClick={() => onRemoveKeep(id)}>delete</button>
                 <button>mail</button>
             </aside>
+            {this.state.showColors && <BgColor onChangeColor={this.props.onChangeColor} keepId={keep.id} />}
+            {/* {this.state.showColors && <BgColor />} */}
         </article>
     }
 }
+
+
+// export class KeepPreview extends React.Component {
+
+//     render() {
+//         const { keep, onRemoveKeep } = this.props
+//         const { type, id } = keep
+
+//         return <article key={id} className="keep-preview flex col space-between">
+//             <section className="keep-body">
+//                 <_DynamicCmp keep={keep} />
+//             </section>
+//             <aside className="keep-tools">
+//                 <button>color</button>
+//                 <button>pin</button>
+//                 <button onClick={() => onRemoveKeep(id)}>delete</button>
+//                 <button>mail</button>
+//             </aside>
+//             {<BgColor />}
+//         </article>
+//     }
+// }
 
 function _DynamicCmp({ keep }) {
     // console.log(type);
